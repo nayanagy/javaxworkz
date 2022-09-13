feec647b949be21aa14243b74bb5fefb04e5a824
unknown <nayanagy.xworkz@gmail.com>
2022-09-13 11:04:11 +0530

    my first push

---
+++ programs/Temperature.java
@@ -0,0 +1,42 @@
+class Temperature{
+	public static void main(String[] a){
+		double temp=28;
+		double distancefrombanglore=0;
+		if(temp==12)
+		{
+			System.out.println("Jammu & Kashmir");
+			distancefrombanglore=2771.3;
+		}
+		if(temp==27)
+		{
+			System.out.println("Bagalkot");
+			distancefrombanglore=474.7;
+		}
+		if(temp==28)
+		{
+			System.out.println("Bellary");
+			distancefrombanglore=312.8;
+		}
+		if(temp==26)
+		{
+			System.out.println("Badravati");
+			distancefrombanglore=281.8;
+		}
+		if(temp==25)
+		{
+			System.out.println("Davangere");
+			distancefrombanglore=264.1;
+		}
+		if(temp==29)
+		{
+			System.out.println("Gulbarga");
+			distancefrombanglore=586;
+		}
+		if(temp==23)
+		{
+			System.out.println("Chikmagalure");
+			distancefrombanglore=242;
+		}
+		System.out.println("distance from Banglore"+distancefrombanglore);
+	}
+}
